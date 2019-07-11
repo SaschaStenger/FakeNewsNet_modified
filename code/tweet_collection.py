adf2de0fec8420f993d9c1f095aa8d8a1d72d856
Sascha Stenger <s.stenger@eckelmann.de>
2019-07-11 15:34:44 +0200

    Changed collect_tweets, so that it now groups the tweets in chunks of up to 100 tweets. Changed dump_tweet_information, so that it will now make calls of up to 100 tweets per call. But still saving every tweet object in a separate file.

--- code/tweet_collection.py
+++ code/tweet_collection.py
@@ -20,17 +20,26 @@ class Tweet:
         self.label = label
 
 
-def dump_tweet_information(tweet: Tweet, config: Config, twython_connector: TwythonConnector):
+def dump_tweet_information(tweet_chunk, config: Config, twython_connector: TwythonConnector):
+    #creating the list of tweet ids
+    tweet_list = []
+    for tweet in tweet_chunk:
+        tweet_list.append(tweet.tweet_id)
+
     try:
-        tweet_object = twython_connector.get_twython_connection(Constants.GET_TWEET).show_status(id=tweet.tweet_id)
+        list_of_tweet_objects = twython_connector.get_twython_connection(Constants.GET_TWEET).lookup_status(id=tweet_list,
+                                                                                                              include_entities= True,
+                                                                                                              map= True)['id']
+        #unrolling the list of tweet objects and saving tweets in single files.
+        for tweet in tweet_chunk:
+            tweet_object = list_of_tweet_objects[str(tweet.tweet_id)]
+            if tweet_object:
+                dump_dir = "{}/{}/{}/{}".format(config.dump_location, tweet.news_source, tweet.label, tweet.news_id)
+                tweet_dir = "{}/tweets".format(dump_dir)
+                create_dir(dump_dir)
+                create_dir(tweet_dir)
 
-        if tweet_object:
-            dump_dir = "{}/{}/{}/{}".format(config.dump_location, tweet.news_source, tweet.label, tweet.news_id)
-            tweet_dir = "{}/tweets".format(dump_dir)
-            create_dir(dump_dir)
-            create_dir(tweet_dir)
-
-            json.dump(tweet_object, open("{}/{}.json".format(tweet_dir, tweet.tweet_id), "w"))
+                json.dump(tweet_object, open("{}/{}.json".format(tweet_dir, tweet.tweet_id), "w"))
 
     except TwythonRateLimitError:
         logging.exception("Twython API rate limit exception")
@@ -51,10 +60,18 @@ def collect_tweets(news_list, news_source, label, config: Config):
     tweet_id_list = []
 
     for news in news_list:
-        for tweet_id in news.tweet_ids:
-            tweet_id_list.append(Tweet(tweet_id, news.news_id, news_source, label))
+        chunk_size = 100
+        tweet_it_chunks = []
+        for i in range(int(len(news.tweet_ids) / chunk_size) + 1):
+            chunk = []
+            for tweet_id in news.tweet_ids[chunk_size * i:chunk_size * (i + 1)]:
+                chunk.append(Tweet(tweet_id, news.news_id, news_source, label))
+            tweet_it_chunks.append(chunk)
+        # for tweet_id in news.tweet_ids:
+        #
+        #     tweet_id_list.append(Tweet(tweet_id, news.news_id, news_source, label))
 
-    multiprocess_data_collection(dump_tweet_information, tweet_id_list, (config, config.twython_connector), config)
+    multiprocess_data_collection(dump_tweet_information, tweet_it_chunks, (config, config.twython_connector), config)
 
 
 class TweetCollector(DataCollector):
